bd30c76d75a8b7d6ee1c6d4ca4c6f213cdc20817
carterave519 <carterave519@gmail.com>
2018-05-08 08:36:53 -0400

    first commit

---
+++ CapTaskList/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapTaskList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate a new Task object
+            Task newTask = new Task("Keith", "Garbage Duty");
+            Task secondTask = new Task("Jacob", "Dishes");
+
+
+            //Print the different Objects
+            Console.WriteLine(newTask.TaskOwner + " "
+                + newTask.TaskName + " " + newTask.DueDate);
+            Console.WriteLine(secondTask.TaskOwner + " "
+                + secondTask.TaskName + " " + newTask.DueDate);
+
+            //create list
+            List<Task> TaskList = new List<Task>();
+
+            //We want to build a new list of tasks, so
+            //  we're going to create a loop
+
+            bool whileBool = false;
+
+            while (whileBool)
+            {
+
+            }
+
+
+
+        }
+    }
+}
+}
---
+++ CapTaskList/Task.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapTaskList
+{
+    class Task
+    {
+            //Classes allow us to make Objects
+            public string TaskOwner { get; set; }
+
+            public string TaskName { get; set; }
+
+            public DateTime DueDate { get; set; }
+
+
+            //Begin creating Constructors
+            //Constructor names should be in Camel Case
+            //We don't leave constructors at the default() because
+            public Task(string taskOwner, string taskName)
+            {
+                //assign the Taskname to the Constructor string
+                TaskOwner = taskOwner;
+                TaskName = taskName;
+                DueDate = DateTime.Now.AddDays(14);
+            }
+
+
+
+        
+    }
+
+
+
+
+}
+
